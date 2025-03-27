09032ac4e7aac4129bfdced26299fbc296a2400b
parhambt <parhambosi@gmail.com>
2025-03-27 20:24:08 +0330

    trying to imlement of recursion logic in body of function

--- src/Q1.cpp
+++ src/Q1.cpp
@@ -7,7 +7,7 @@ int main()
 {
     int n ; 
     cin>> n ;
-    string a = latex_fraction(1);
+    string a = latex_fraction(n);
     cout<<a<<endl ;
 }
 string latex_fraction(int n,string answer,int count)
@@ -16,7 +16,8 @@ string latex_fraction(int n,string answer,int count)
     if(count==0)
     {
         answer=answer+"1";
-        
     }
-    return latex_fraction(n,answer,count+1) ;
+    string numerator=latex_fraction(2*count,answer,count+1);
+    string denumerator=latex_fraction((2*count)+1,answer,count+1);
+    return answer+to_string(count)+"+\frac{"+numerator+"}{"+denumerator+"}";
 }
