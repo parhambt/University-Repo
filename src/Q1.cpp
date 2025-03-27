7a1584508bc442ba99982aff6e2749b2f2e602ac
parhambt <parhambosi@gmail.com>
2025-03-27 19:58:43 +0330

    implement base case

--- src/Q1.cpp
+++ src/Q1.cpp
@@ -1,3 +1,22 @@
-int main() {
-    return 0;
+#include <iostream>
+
+using namespace std ; 
+string latex_fraction(int n,string answer="",int count=0);
+
+int main()
+{
+    int n ; 
+    cin>> n ;
+    string a = latex_fraction(1);
+    cout<<a<<endl ;
+}
+string latex_fraction(int n,string answer,int count)
+{
+    if(n==count) return answer;
+    if(count==0)
+    {
+        answer=answer+"1";
+        
+    }
+    return latex_fraction(n,answer,count+1) ;
 }
