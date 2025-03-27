069b8e92e1a12bd00cb9dbc8e9377e5fe6776c20
parhambt <parhambosi@gmail.com>
2025-03-27 23:48:13 +0330

    Q2.implementing find oldest index

--- src/Q2.cpp
+++ src/Q2.cpp
@@ -1,3 +1,15 @@
-int main() {
-    return 0;
+#include <iostream>
+#include <vector>
+using namespace std ;
+int find_oldest_index(const vector<int> &ages,int oldest_index=0,int cur_index=0);
+int main()
+{
+    vector<int> ages={1,4,3,2,5};
+    cout<<find_oldest_index(ages)<<endl ;
+}
+int find_oldest_index(const vector<int> &ages,int oldest_index,int cur_index)
+{
+    if(ages.size()==cur_index) return oldest_index ;
+    if(ages[cur_index]>ages[oldest_index]) oldest_index=cur_index ;
+    return find_oldest_index(ages,oldest_index,cur_index+1);
 }
