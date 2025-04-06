6706a11f60e0f30f8b63733e4a3fbda0523cc154
parhambt <parhambosi@gmail.com>
2025-04-06 22:34:22 +0330

    Q4.adding input handelling

--- src/Q4.cpp
+++ src/Q4.cpp
@@ -1,3 +1,29 @@
-int main() {
-    return 0;
+#include <iostream>
+#include <vector>
+using namespace std ; 
+
+
+
+
+
+int main()
+{
+    vector<string> map ; 
+    map = input_handelling() ; 
+
+}
+
+vector<string> input_handelling()
+{
+    int x , y ; 
+    cin>>x>>y ; 
+    vector<string> map(x) ;
+    for(int i=0 ; i<x;i++)
+    {
+        string line ; 
+        cin>>line ; 
+        map[i] = line ; 
+    } 
+    return map ; 
+
 }
