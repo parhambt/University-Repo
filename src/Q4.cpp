acacc61077fe0b3692ee5270ff1699ebbc3d9e87
parhambt <parhambosi@gmail.com>
2025-04-07 17:58:32 +0330

    Q4.imlement find possible path function

--- src/Q4.cpp
+++ src/Q4.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 using namespace std ; 
 
+const char SEA='~' ; 
+const char MOUNTAIN='#' ;
+const char SEAPORT='%' ;
+const vector<char> LANDS = {'0','1','2','3','4','5','6','7','8','9'} ;
 
 vector<string> input_handelling(pair<int,int> &max_row_col);
 vector<pair<int,int>> find_land_index(const vector<string> &map,char target,pair<int,int> max_row_col,vector<pair<int,int>> &answer,int row=0,int col=0) ;
@@ -17,6 +21,38 @@ int main()
     int b ; 
 
 
+}
+void find_direction(const vector<string> &map , const vector<pair<int,int>> &targget_land_index ,pair<int,int> current_index,char target,vector<vector<pair<int,int>>> all_direction , vector<pair<int,int>> one_direction)
+{
+    vector<char> possible_path=find_possible_path(current_index,map,target) ;
+    if()
+}
+vector<char> find_possible_path(pair<int,int> current_index,const vector<string> &map,char target)
+{
+    vector<char> avaleble_path ; 
+    auto is_land=[](pair<int,int> current_index , const vector<string> &map)
+    {
+        if(map[current_index.first][current_index.second]>=48 && map[current_index.first][current_index.second]<=57) return true ; 
+        else return false ; 
+    } ;
+
+    if(is_land(current_index,map)) // it is in land
+    {
+        avaleble_path.push_back(target) ; 
+        avaleble_path.push_back(SEAPORT) ;
+    }
+    else if(map[current_index.first][current_index.second]==SEAPORT)// it is not currently on land
+    {
+        avaleble_path.push_back(SEA) ;
+        avaleble_path.push_back(target) ;
+        avaleble_path.push_back(SEAPORT) ; 
+    }
+    else if(map[current_index.first][current_index.second]==SEA)
+    {
+        avaleble_path.push_back(SEAPORT) ; 
+        avaleble_path.push_back(SEA) ;
+    }
+    return avaleble_path ;
 }
 vector<pair<int,int>> find_land_index(const vector<string> &map,char target,pair<int,int> max_row_col,vector<pair<int,int>> &answer,int row,int col)
 {
