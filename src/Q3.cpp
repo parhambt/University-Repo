7dd778c6a8b1a5dc8d0aeb8fcb8a0a322722ecee
parhambt <parhambosi@gmail.com>
2025-04-12 00:19:50 +0330

    Q3.imlementing output handelling function

--- src/Q3.cpp
+++ src/Q3.cpp
@@ -8,16 +8,24 @@ void input_handelling(map<string,int> &info,map<string,int> &menu_map , vector<s
 int sumBill(map<string,int> menu , vector<string> input,bool &flag,map<string,int> count_item)  ; 
 void allPosibelityOrder(map<string,int> &menu_map,vector<string> menu_list,vector<pair<vector<string>,int>> &all_posiblities,vector<string> possibelity={},int possibelity_price=0) ;
 pair<int,int> findBoundPrice( map<string,int> &info) ; 
-void checkCondition(map<string,int> info,map<string,int> menu_map , const vector<pair<vector<string>,int>> &all_posiblities,pair<int,int> price_bound,pair<vector<string>,int> &answer, int index ) ; 
+void checkCondition(map<string,int> info,map<string,int> menu_map , const vector<pair<vector<string>,int>> &all_posiblities,pair<int,int> price_bound,pair<vector<string>,int> &answer, int index=0 ) ; 
+void output_handelling(const vector<string> &foods ) ;
 
 int main()
 {
+    // map<string,int> info={{"count_menu",3},{"discount%",50},{"least_money_discountApply",500},{"max_discount_value",310}};
+    // map<string,int> menu_map={{"Juicy_burger",250},{"Fettuccine_Alfredo",300},{"Coca_Cola",30}} ;
+    // vector<string> menu_list={"Juicy_burger","Fettuccine_Alfredo","Coca_Cola"} ; 
     map<string,int> info;
-    map<string,int> menu_map ;
+    map<string,int> menu_map;
     vector<string> menu_list ; 
     input_handelling(info,menu_map,menu_list);
-   
-
+    vector<pair<vector<string>,int>> all_posiblities ;
+    allPosibelityOrder(menu_map,menu_list,all_posiblities) ; 
+    pair<vector<string>,int> best_order = {{},-1} ; 
+    pair<int,int> price_bound = findBoundPrice(info) ; 
+    checkCondition(info,menu_map,all_posiblities,price_bound,best_order) ;
+    output_handelling(best_order.first) ;
 }
 void allPosibelityOrder(map<string,int> &menu_map,vector<string> menu_list,vector<pair<vector<string>,int>> &all_posiblities,vector<string> possibelity,int possibelity_price)
 {
@@ -47,7 +55,8 @@ void checkCondition(map<string,int> info,map<string,int> menu_map , const vector
 pair<int,int> findBoundPrice( map<string,int> &info)
 {
     int low_bound = info["least_money_discountApply"] ;
-    int up_bound = (1/(info["discount%"]/100)) * (info["max_discount_value"]) ; 
+    double ratio = info["discount%"]/100.0 ; 
+    int up_bound = (1/ratio) * (info["max_discount_value"]) ; 
     return make_pair(low_bound,up_bound) ;
 }
 
@@ -82,5 +91,13 @@ void input_handelling(map<string,int> &info,map<string,int> &menu_map , vector<s
         menu_map.insert({food_name,price});
         menu_list.push_back(food_name);
     }
+
+}
+void output_handelling(const vector<string> &foods )
+{
+    for(auto& food:foods)
+    {
+        cout<<food<<endl ;
+    } 
 
 }
