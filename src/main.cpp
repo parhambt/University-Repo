9c650da77aa03c8475874b4db02d1ef4cae04737
parhambt <parhambosi@gmail.com>
2025-05-03 17:16:20 +0330

    implement chosse question method in Question class that sort question by score

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <fstream>
 #include <sstream>
+#include<algorithm>
 using namespace std;
 
 
@@ -60,29 +61,117 @@ public :
             {
                 Exam::add_template(parse_template_data(current_line)) ; 
             }
+            else if (input==GENERATE_TEST)
+            {
+                string test_name , template_name ; 
+                test_name=IO::parse_word_in_quote(current_line) ; 
+                template_name=IO::parse_word_in_quote(current_line) ;
+                
+            }
+            
         }
     }
-    static map<string , vector<vector<string>>> parse_template_data(istringstream & given_data)
+    static map<string , vector<vector<string>>> parse_template_data(istringstream & given_line)
     {
         string single_data ,template_name; 
-        char single_quote ; 
         vector<vector<string>> parsed_data ; 
-        given_data>>ws>>single_quote ; 
-        getline(given_data , template_name , '\'') ; 
-        given_data>>ws ; 
-        while(getline(given_data,single_data,' '))
+        template_name=IO::parse_word_in_quote(given_line) ; 
+        while(getline(given_line,single_data,' '))
         {
             istringstream exam_detail(single_data) ; 
             string subj , difficulty , count ;
             getline(exam_detail,subj,':') ; 
             getline(exam_detail,difficulty,':') ; 
-            getline(exam_detail,count,' ') ; 
+            getline(exam_detail,count,' ') ;  
             parsed_data.push_back({subj,difficulty,count}) ; 
         }
         return {{template_name,parsed_data}}  ;
     }
+    static string parse_word_in_quote(istringstream & given_line)
+    {
+        string target_word  ; 
+        char single_quote ;
+        given_line>>ws>>single_quote ; 
+        getline(given_line , target_word , '\'') ;
+        given_line>>ws ; 
+        return target_word ; 
+    }
 };
 
+class Questions
+{
+private : 
+    int num_blanks , num_corrects , num_incorrect , priority  ;
+    string question_text,difficulty,subject,option1,option2,option3,option4,correct_answer;
+    inline static map<pair<string,string> , vector<Questions * >> all_questions={} ; 
+public : 
+     
+     
+    Questions(string question_text , string difficulty , string subject , string option1 , string option2 , string option3 , string option4,string correct_answer)
+    {
+        this->question_text = question_text ; 
+        this->difficulty = difficulty ; 
+        this->subject =subject ; 
+        this->option1 = option1 ; 
+        this->option2 = option2 ;
+        this->option3 = option3 ;
+        this->option4 = option4 ;
+        this->correct_answer = correct_answer ; 
+        this->num_blanks = 0 ; 
+        this->num_corrects = 0 ; 
+        this->num_incorrect = 0  ; 
+        this->priority = 0 ; 
+        Questions::all_questions[make_pair(subject,difficulty)].push_back(this) ; 
+
+    }
+    static map<pair<string,string> , vector<Questions * >>& get_all_questions()
+    {
+        return Questions::all_questions ; 
+    }
+    void set_num_blanks(int blanks)
+    {
+        this->num_blanks = blanks ; 
+        priority_calculator() ; 
+
+    }
+    void set_num_incorrects(int incorrects)
+    {
+        this->num_incorrect = incorrects ; 
+        priority_calculator() ; 
+    }
+    void set_num_corrects(int corrects)
+    {
+        this->num_corrects = corrects ;
+        priority_calculator() ; 
+    }
+    void priority_calculator()
+    {
+        int priority = 3*(this->num_incorrect)   + this->num_blanks - 2*(this->num_corrects)  ;
+        this->priority = priority ; 
+    }
+    static vector<Questions * > choose_question_by_priority(string subject , string difficulty,int count)
+    {
+        vector<Questions * > & all_questions = Questions::get_all_questions()[make_pair(subject , difficulty)] ; 
+        auto sort_priority = [](Questions * a , Questions * b)
+        {
+            if((a->priority!=b->priority ))
+            {
+                if(a->priority > b->priority) return true ;
+                else return false ; 
+            }
+            else
+            {
+                if(a->question_text < b->question_text) return true ; 
+                else return false ; 
+            }
+        } ; 
+        sort(all_questions.begin(),all_questions.end(),sort_priority) ; 
+        vector<Questions * > choosen_questions (all_questions.begin(),all_questions.begin()+count) ; 
+        return choosen_questions ; 
+    }
+
+   
+};
 
 
 class Csv
@@ -117,51 +206,7 @@ public:
 } ; 
 
 
-class Questions
-{
-private : 
-    int num_blanks , num_corrects , num_incorrect , priority  ;
-    string question_text,difficulty,subject,option1,option2,option3,option4,correct_answer;
-    static vector<Questions *> all_questions ; 
-public : 
-     
-     
-    Questions(string question_text , string difficulty , string subject , string option1 , string option2 , string optio3 , string option4,string correct_answer)
-    {
-        this->question_text = question_text ; 
-        this->difficulty = difficulty ; 
-        this->subject =subject ; 
-        this->option1 = option1 ; 
-        this->option2 = option2 ;
-        this->option3 = option3 ;
-        this->option4 = option4 ;
-        this->correct_answer = correct_answer ; 
-        this->num_blanks = 0 ; 
-        this->num_corrects = 0 ; 
-        this->num_incorrect = 0  ; 
-        this->priority = 0 ; 
-        all_questions.push_back(this) ;
 
-    }
-    void set_num_blanks(int blanks)
-    {
-        this->num_blanks = blanks ; 
-    }
-    void set_num_incorrects(int incorrects)
-    {
-        this->num_incorrect = incorrects ; 
-    }
-    void set_num_corrects(int corrects)
-    {
-        this->correct_answer = corrects ; 
-    }
-    static vector<Questions * > select_question_by_priority()
-    {
-
-    }
-
-   
-};
 
 
 int main(int argc , char * argv[])
